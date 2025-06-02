ab6117f516fe57d059b130f471ad9b2971de3012
Dutkercz <dutkercz@gmail.com>
2025-06-02 16:20:05 -0300

    Valor de issuer no app.properties

--- backend/project-login-app/src/main/java/Dutkercz/com/github/project_login_app/services/JwtService.java
+++ backend/project-login-app/src/main/java/Dutkercz/com/github/project_login_app/services/JwtService.java
@@ -8,12 +8,16 @@ import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
 @Service
 public class JwtService {
 
   @Value("${alg.key}")
   private String algKey;
 
+  @Value("${issuer}")
   private String issuer;
 
   public String jwtCreate(Usuario usuario){
@@ -22,6 +26,7 @@ public class JwtService {
       return JWT.create()
         .withIssuer(issuer)
         .withSubject(usuario.getUsername())
+        .withExpiresAt(Instant.now().plus(2, ChronoUnit.HOURS))
         .sign(algorithm);
     } catch (IllegalArgumentException | JWTCreationException e){
       throw new JWTCreationException(e.getMessage(), e);
